eeadd1ea4a752d2a4e32814116b812ae187a475d
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-03-05 16:23:50 -0300

    Day 1 - Criado a Estutura em HTML da página - Criado o Script para os botões Adicionar e   Remover - integrado o script JS no HTML - Realizado os testes nas funções addItems() e   removeItems()

--- Readme.md
+++ Readme.md
@@ -1,5 +1,6 @@
 # To do list 
 ## Objetivos:
-- [ ] Criar uma lista de afazeres utilizando HTML e CSS
-- [ ] Utilizar o JS para fazer a inclusão e remoção de itens dinâmicamente
-- [ ] Utilizar a interface Window.localStorage() para amarzenar os dados.
+- [X] Criar uma lista de Afazeres utilizando HTML e CSS
+- [X] Utilizar o JS para fazer a inclusão e remoção de itens dinâmicamente
+- [ ] Utilizar a interface Window.localStorage( ) para armazenar os dados.
+- [ ] Fazer os Style da Página no CSS.
---
+++ script.js
@@ -0,0 +1,44 @@
+
+/*
+Botão para adicionar itens
+*/
+var btnInsert = document.getElementById("btnInserir");
+var btnRemove = document.getElementById("btnRemover");
+var cont = 0; //Variárivel para contar os elementos Filhos de #itens
+
+btnInsert.addEventListener("click",addItems);
+
+function addItems() {
+  //criação de objetos para instanciar alguns elementos da página Index.html
+
+  
+  
+  //seleciona o <input> "type=text" #textTarefaValor
+  const txtTaskValue = document.getElementById("textTarefaValor");
+
+  //cria uma variável que recebe o elemento <p>
+  const task = document.createElement("p");
+
+  //recuperando o valor do input  
+  task.innerText = txtTaskValue.value;
+
+  //adiciona filhos a div #itens
+  document.getElementById("itens").appendChild(task);
+  cont++; //adiona mais um na contagem;
+}
+
+/*
+botão para remover itens
+*/
+
+btnRemove.addEventListener("click",removeItems);
+
+
+function removeItems() {
+
+  var childOfItens = document.getElementById("itens").childNodes;
+
+  document.getElementById("itens").removeChild(childOfItens[cont]);
+  cont--;
+
+}
