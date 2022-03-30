49de40ff2b83220441f85ab69c468e165cf3a05f
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-03-29 22:21:31 -0300

    funcition save()

--- script.js
+++ script.js
@@ -6,24 +6,24 @@ var btnInsert = document.getElementById("btnInserir");
 var btnRemove = document.getElementById("btnRemover");
 var cont = 0; //Variárivel para contar os elementos Filhos de #itens
 i = 0;
+storedNames = 0;
 
 var tasks = [];
 
-function addTask(text) {
-  tasks.push(text);
-  localStorage.setItem("tasks",JSON.stringify(tasks))
 
-   storedNames = JSON.parse(localStorage.getItem("tasks")); 
+storedNames = JSON.parse(localStorage.getItem("tasks")); 
+if(storedNames!=null)
+storedNames.forEach(element => {
+  tasks.push(element); 
+  update()
+});
 
-}
-
-
-
-function update(task) {
-  task.innerHTML = storedNames[i];
-  document.getElementById("itens").appendChild(task);
-  i++;
-}
+// function update() {
+//   const task = document.createElement("li");
+//     task.innerHTML = tasks[element];
+//   document.getElementById("itens").appendChild(task);
+  
+// }
 
 
 
@@ -33,53 +33,21 @@ btnInsert.addEventListener("click", () => {
   const txtTaskValue = document.getElementById("textTarefaValor");
 
 
-  const task = document.createElement("li");
   
-  addTask("- " + txtTaskValue.value);
-  update(task);
- 
   
+  text = "- " + txtTaskValue.value;
+  
+  console.log(tasks)
+  save(text, tasks)
 })
 
-
+function save() {
+tasks.push(text);
+localStorage.setItem("tasks",JSON.stringify(tasks))
+}
 
 //arrow function https://www.youtube.com/watch?v=S5Mn0qQzJ-0
 btnRemove.addEventListener("click",() => {
   const list =  document.getElementById("itens");
   document.getElementById("itens").removeChild(list.lastChild);
 })
-
-
-/*
-fazer depois:
-- Criar função dentro do <script> do html
-- Criar uma class para o Toggle no CSS, contendo a propriedade "text-decoration: line-through"
-- Na função adicionar a condição : 
-SE (elemento = clicado) então {
-                                var element = document.getElementById("idElemento");
-                                element.classList.toggle("mystyle");
-                                  }
-Documentação:
-How TO - Toggle Class
-link <https://www.w3schools.com/howto/howto_js_toggle_class.asp>
-
-Outras informações:
-Fórum: stackoverflow
-Link <https://pt.stackoverflow.com/>
-Questão:
-"Como saber se um elemento foi clicado usando Javascript puro"
-Link da pergunta respondida <https://pt.stackoverflow.com/questions/11365/como-saber-se-um-elemento-foi-clicado-usando-javascript-puro#:~:text=Uma%20outra%20op%C3%A7%C3%A3o%20%C3%A9%20adicionar,target%20.&text=Utilize%20o%20document.>
-*/
-// window.onload = function() {
-// 	var allTasks = document.getElementsByTagName("ul");
-  
-// 	for(var i=0; i<allTasks.length; i++) {
-// 		allTasks[i].addEventListener("click",() =>{
-//     allTasks[i].classList.add("tksline-through")
-      
-//     alert('o elemento foi clicado');
-//     console.log("o elemento foi clicado");
-//     })
-//   }
-// }  
-  
