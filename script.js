9ec3b23db17c7b051afeb73ecde0023a10c8fc50
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-03-31 21:10:05 -0300

    alteração em algumas funções - declaração de variaveis para as funções de remoção e adição de tarefas - upadate na na função de remoção de tarefas

--- script.js
+++ script.js
@@ -1,56 +1,48 @@
-
 /*
-Botão para adicionar itens
+Botões
 */
 var btnInsert = document.getElementById("btnInserir");
 var btnRemove = document.getElementById("btnRemover");
-var cont = 0; //Variárivel para contar os elementos Filhos de #itens
-// i = 0;
+
 storedNames = 0;
 
 var tasks = [];
 
-
-storedNames = JSON.parse(localStorage.getItem("tasks")); 
+storedNames = JSON.parse(localStorage.getItem("tasks"));
 if (storedNames != null)
-storedNames.forEach(function(element,index){
-  tasks.push(element); 
-  i = index;
-  update(i);
-});
+  storedNames.forEach(function (element, index) {
+    tasks.push(element);
+    i = index;
+    update(i);
+  });
 
 function update() {
   const task = document.createElement("li");
-    task.innerHTML = tasks[i];
+  task.innerHTML = tasks[i];
   document.getElementById("itens").appendChild(task);
-  
+
 }
 
+const insertTask = btnInsert.addEventListener("click", () => {
 
-btnInsert.addEventListener("click", () => {
- 
   const txtTaskValue = document.getElementById("textTarefaValor");
   text = "- " + txtTaskValue.value;
-  
+
   console.log(tasks)
   save(text, tasks)
 })
 
 function save() {
-tasks.push(text);
-localStorage.setItem("tasks",JSON.stringify(tasks))
-setTimeout(() => window.location.reload(), 2000);
+  tasks.push(text);
+  localStorage.setItem("tasks", JSON.stringify(tasks))
+  setTimeout(() => window.location.reload(), 500);
 }
 
-//arrow function https://www.youtube.com/watch?v=S5Mn0qQzJ-0
-btnRemove.addEventListener("click",() => {
-  const list =  document.getElementById("itens");
+const removeTask = btnRemove.addEventListener("click", () => {
+  const list = document.getElementById("itens");
   document.getElementById("itens").removeChild(list.lastChild);
+  console.log(tasks)
+  tasks.splice(-1, 1);
+  localStorage.setItem("tasks", JSON.stringify(tasks))
 })
 
-
-/*
-Array forEach()
-https://www.w3schools.com/jsref/jsref_forEach.asp
-https://www.youtube.com/watch?v=zF48zb631Lg
-*/
