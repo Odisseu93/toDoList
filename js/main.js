a7e947d618744f430391656913f4997943282319
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-07-17 10:29:57 -0300

    adicionando funcao para adicionar tarefa pela tecla enter - nova funcao para adicao de tarefas - processo de validacao para valor vazio no input

--- js/main.js
+++ js/main.js
@@ -3,18 +3,54 @@ const toDoList = document.querySelector('ul');
 const btnInserir = document.querySelector('#btnInserir');
 
 
-// função para inserir a tarefa
+// função para inserir a tarefa pelo botão
 btnInserir.addEventListener('click', () => {
+  const valorInput = input.value;
 
-  const tarefa = new Tarefa(input);
-  const tarefaHTML = `
+  if (valorInput != '') {
+    const tarefa = new Tarefa(input);
+    const tarefaHTML = `
   <li class=\"tks\"> - ${tarefa.mensagem}</li> 
   `
 
-  atualizarUl(tarefaHTML);
+    atualizarUl(tarefaHTML);
 
-  input.value = ''; //limpando o input 
-  input.focus(); //autofocus
+    input.value = ''; //limpando o input 
+    input.focus(); //autofocus
+  } else {
+    const erroEntradaIvalida = 'insira um valor na caixa de entrada !';
+    alert(erroEntradaIvalida); //provisório
+  }
+});
+
+
+// função para inserir a tarefa pelo botão
+input.addEventListener('keypress', (e) => {
+  const valorInput = e.target.value;
+  const tecla = e.key;
+
+  if ((tecla === 'Enter') && (valorInput != '')) { // insere uma tarefa caso o input não esteja vazio
+    e.preventDefault();
+    const tarefa = new Tarefa(input);// instancia da classe Tarefa
+    const tarefaHTML = `
+    <li class=\"tks\"> - ${tarefa.mensagem}</li> 
+    `
+
+    atualizarUl(tarefaHTML);
+
+    input.value = ''; //limpando o input 
+    input.focus(); //autofocus
+  } else {
+    const erroEntradaIvalida = 'insira um valor na caixa de entrada !';
+    /*
+    exibindo como erro no console.log
+    e.preventDefault();
+    console.error(erroEntradaIvalida); 
+    throw new Error(erroEntradaIvalida) ; //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/throw
+    */
+
+    alert(erroEntradaIvalida); //provisório
+  }
 });
 
 
