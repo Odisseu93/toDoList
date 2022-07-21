f6e53de15d8e44dc6179d5e88e31f2904e4e9db3
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-07-20 22:25:29 -0300

    validacao para evitar duplicidade nas tarefas

--- js/inserir-tarefas.js
+++ js/inserir-tarefas.js
@@ -1,9 +1,9 @@
 // função para inserir a tarefa pelo botão
 btnInserir.addEventListener('click', () => {
   const valorInput = input.value;
+  const tarefa = new Tarefa(input);
 
-  if (valorInput != '') {
-    const tarefa = new Tarefa(input);
+  if ((valorInput != '') && (tarefa.tarefaEhvalida())) {
     const tarefaHTML = `
   <li class=${tarefa.class}>- ${tarefa.mensagem} </li> 
   `
@@ -12,13 +12,30 @@ btnInserir.addEventListener('click', () => {
 
     input.value = ''; //limpando o input 
     input.focus(); //autofocus
-  } else {
-    const input = document.querySelector('#textTarefaValor');
-    const mensagemOriginal = 'Insira a sua tarefa aqui!'
+  } 
+//tarefas repididas
+  else if((!tarefa.tarefaEhvalida()) && (valorInput !='')) { 
+    // e.preventDefault();
+    const mensagemOriginal = 'Insira a sua tarefa aqui!';
+    const erroTarefaJahExiste= 'Esta tarefa já existe!';
+
+    input.value = '';
+    input.placeholder = erroTarefaJahExiste
+    input.classList.add('invalid');
+
+    setTimeout( ()=>{
+      input.classList.remove('invalid');
+      input.placeholder = mensagemOriginal;
+    }
+    ,2000)// tempo seta em 2s
+
+  } 
+// input vazio
+   else if ((valorInput === '')) {
+    const mensagemOriginal = 'Insira a sua tarefa aqui!';
     const erroEntradaInvalida = 'Insira uma Tarefa!';
 
-    //monstra mensagem no placeholder e faz uma animação no input
-    input.placeholder = erroEntradaInvalida
+    input.placeholder = erroEntradaInvalida;
     input.classList.add('invalid');
 
     setTimeout( ()=>{
@@ -36,8 +53,8 @@ input.addEventListener('keypress', (e) => {
   const valorInput = e.target.value;
   const tecla = e.key;
   const tarefa = new Tarefa(input);// instancia da classe Tarefa
-
-  if ((tecla === 'Enter') && (valorInput != '')) { // insere uma tarefa caso o input não esteja vazio
+  
+  if ((tecla === 'Enter') && (valorInput != '') && (tarefa.tarefaEhvalida())) { // insere uma tarefa caso o input não esteja vazio
     e.preventDefault();
     const tarefaHTML = `
     <li class=${tarefa.class}>- ${tarefa.mensagem} </li> 
@@ -46,9 +63,27 @@ input.addEventListener('keypress', (e) => {
 
     input.value = ''; //limpando o input 
     input.focus(); //autofocus
-  } else if ((tecla === 'Enter') && (valorInput === '')) {
+  }
+  //tarefas repididas
+  else if((tecla === 'Enter') && (!tarefa.tarefaEhvalida()) && (input.value !='')) { 
     e.preventDefault();
-    const input = document.querySelector('#textTarefaValor');
+    const mensagemOriginal = 'Insira a sua tarefa aqui!';
+    const erroTarefaJahExiste = 'Esta tarefa já existe!';
+
+    input.value = '';
+    input.placeholder = erroTarefaJahExiste;
+    input.classList.add('invalid');
+
+    setTimeout( ()=>{
+      input.classList.remove('invalid');
+      input.placeholder = mensagemOriginal;
+    }
+    ,2000)// tempo seta em 2s
+
+  } 
+  // input vazio
+   else if ((tecla === 'Enter') && (valorInput === '')) {
+    e.preventDefault();
     const mensagemOriginal = 'Insira a sua tarefa aqui!';
     const erroEntradaInvalida = 'Insira uma Tarefa!';
 
--- js/tarefa.js
+++ js/tarefa.js
@@ -2,7 +2,16 @@
 class Tarefa {
   constructor(input) {
     this.mensagem = input.value;
-    this.class = "ToDoList__lista-tarefas__item"
+    this.class = "ToDoList__lista-tarefas__item";
+  }
+  
+  tarefaEhvalida() {
+    const reg = new RegExp('(\\s)' + this.mensagem + '(\\s)', 'i');
+    //https://stackoverflow.com/questions/17863066/why-do-regex-constructors-need-to-be-double-escaped
+
+    if (toDoList.textContent.search(reg) < 0) return true
+    else return false
+
   }
 }
 
